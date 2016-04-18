578af0bce5c8a053cf1dfb43bc00642c7495c213
sebavides <sebavides@gmail.com>
2016-04-17 22:36:49 -0300

    2

--- peticionServidor2semana/ViewController.swift
+++ peticionServidor2semana/ViewController.swift
@@ -14,13 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var bookTitle: UILabel!
     @IBOutlet weak var bookAutor: UILabel!
     @IBOutlet weak var alerta: UILabel!
-    
+    @IBOutlet weak var cover: UIImageView!
     
     func sincrono(){
         let isbn = textField.text
         let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn!)"
         let url = NSURL(string: urls)
         let datos :NSData? = NSData(contentsOfURL: url!)
+        
+        
+        
         if datos == nil {
             let alerta = UIAlertController(title: "Alerta", message: "No dispones de conexion a internet", preferredStyle: .Alert)
             alerta.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
@@ -30,41 +33,53 @@ class ViewController: UIViewController {
             let alerta = UIAlertController(title: "Alerta", message: "No has ingresado un ISBN", preferredStyle: .Alert)
             alerta.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
             self.presentViewController(alerta, animated: true, completion: nil)
-        }else{
+        }else if datos != nil{
             do {
                 let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
                 let dic1 = json as! NSDictionary
+                    if dic1.count == 0 {
+                    cover.image = nil
+                    bookTitle.text = "No se encontró el libro"
+                    return
+                    }
                 let dic2 = dic1["ISBN:\(isbn!)"] as! NSDictionary
                 self.bookTitle.text = dic2["title"] as! NSString as String
                 let dic3 = dic2["authors"] as! NSArray
                 let dic4 = dic3[0] as! NSDictionary
                 self.bookAutor.text = dic4["name"] as! NSString as String
                 
+                let imageURL = NSURL(string: "http://covers.openlibrary.org/b/isbn/\(isbn!)-L.jpg")!
+                let image = NSData(contentsOfURL: imageURL)!
+                cover.image = UIImage(data: image)
                 
             }catch _{
-                
-            }
+                    }
+                }
         }
-    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
     }
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+     //trate de utilizar este metodo pero no funciono
+    @IBAction func textFieldReturn(sender: AnyObject) {
+        self.resignFirstResponder()
+        sincrono()
+    }
     
+    //por eso he creado este boton
     @IBAction func buscarButton() {
         sincrono()
     }
-
-    @IBAction func buscar(sender: AnyObject) {
-        sincrono()
-    }
-   
-
 }
     
     
